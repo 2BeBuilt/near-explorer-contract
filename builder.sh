e42007ed624e18844a5abcdfd5f03b504ec5f242
Andrey Gruzdev <agruzdev1@gmail.com>
2023-12-04 23:25:03 +0100

    update builder

--- builder.sh
+++ builder.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-CONTAINER_NAME="sourcescan-builder-rust"
+DEFAULT_IMAGE="nearprotocol/contract-builder:latest-amd64"
 
-# Check if the container already exists
-if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
-    echo "Container with name ${CONTAINER_NAME} already exists."
+IMAGE_NAME=${1:-$DEFAULT_IMAGE}
 
-    # Check if the container is running
-    if docker ps --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
-        echo "Container is already running."
-    else
-        echo "Starting existing container."
-        docker start "${CONTAINER_NAME}"
-        docker exec -it "${CONTAINER_NAME}" /bin/bash
-    fi
-else
-    echo "Creating and starting a new container."
-    docker run \
-        --name "${CONTAINER_NAME}" \
-        --mount type=bind,source=${HOST_DIR:-$(pwd)/..},target=/host \
-        --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-        -i -t nearprotocol/contract-builder:latest-amd64 \
-        /bin/bash
-fi
+docker run \
+    --mount type=bind,source=${HOST_DIR:-$(pwd)/..},target=/host \
+    --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+    -i -t $IMAGE_NAME \
+    /bin/bash
