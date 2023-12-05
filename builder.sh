6ee724a06303cb0805f2e2c53914eead9ffffc49
Andrey Gruzdev <agruzdev1@gmail.com>
2023-12-05 17:06:53 +0100

    update builder

--- builder.sh
+++ builder.sh
@@ -9,7 +9,7 @@ IMAGE_NAME=${1:-$DEFAULT_IMAGE}
 
 # Function to check if a container exists
 container_exists() {
-  docker ps -a --format '{{.Names}}' | grep -w $CONTAINER_NAME > /dev/null 2>&1
+  docker ps -a --format '{{.Names}}' | grep -qw $CONTAINER_NAME
 }
 
 # Function to get the image of an existing container
@@ -19,22 +19,29 @@ get_container_image() {
 
 # Check if the container exists
 if container_exists; then
+  echo "Container $CONTAINER_NAME already exists."
+  
   # Check if the existing container was created with a different image
   if [ "$(get_container_image)" != "$IMAGE_NAME" ]; then
-    echo "Container exists with a different image, recreating..."
-
-    # Stop and remove the existing container
-    docker stop $CONTAINER_NAME > /dev/null 2>&1
-    docker rm $CONTAINER_NAME > /dev/null 2>&1
+    echo "Existing container was created with a different image. Updating image..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+    docker run \
+        --name $CONTAINER_NAME \
+        --mount type=bind,source=${HOST_DIR:-$(pwd)/..},target=/host \
+        --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+        -it $IMAGE_NAME \
+        /bin/bash
   else
-    echo "Container already exists with the same image, remounting..."
+    echo "Reusing existing container with the same image."
+    docker start -ai $CONTAINER_NAME
   fi
+else
+  echo "Creating a new container..."
+  docker run \
+      --name $CONTAINER_NAME \
+      --mount type=bind,source=${HOST_DIR:-$(pwd)/..},target=/host \
+      --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+      -it $IMAGE_NAME \
+      /bin/bash
 fi
-
-# Run the Docker container
-docker run \
-    --name $CONTAINER_NAME \
-    --mount type=bind,source=${HOST_DIR:-$(pwd)/..},target=/host \
-    --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-    -i -t $IMAGE_NAME \
-    /bin/bash
